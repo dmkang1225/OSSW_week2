780abf4f9c04996840dccedc4adbb2acc9442bb9
Dongmin Kang <dmkang1225@naver.com>
2025-03-31 02:49:22 +0900

    fix handle non-numeric input

--- calcuate.py
+++ calcuate.py
@@ -18,14 +18,24 @@ def divide(a, b):
 if __name__ == "__main__":
 
     ### 사용자 입력
-    print("\n첫번째 숫자를 입력하세요.")
-    input1 = float(input("입력: "))
+    while True:
+        try:
+            print("\n첫번째 숫자를 입력하세요.")
+            input1 = float(input("입력: "))
+            break
+        except ValueError:
+            print("유효한 숫자를 입력하세요.")
 
     print("\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)")
     act = input("입력: ")
 
-    print("\n두번째 숫자를 입력하세요.")
-    input2 = float(input("입력: "))
+    while True:
+        try:
+            print("\n두번째 숫자를 입력하세요.")
+            input2 = float(input("입력: "))
+            break
+        except ValueError:
+            print("유효한 숫자를 입력하세요.")
 
     ### 연산 수행
     if act == "+":
